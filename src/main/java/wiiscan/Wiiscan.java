0ceab0a316effcfb453aac07b4ba2f153076c5a9
Rik Veenboer <rik.veenboer@gmail.com>
2016-08-20 15:40:03 +0100

    Add pre-built binary and dependency to source control

--- src/main/java/wiiscan/Wiiscan.java
+++ src/main/java/wiiscan/Wiiscan.java
@@ -3,8 +3,13 @@ package wiiscan;
 import com.github.boukefalos.jlibloader.Native;
 
 public class Wiiscan {
-	public static void load() {
+	public static String load() {
+		Native.load("com.github.boukefalos", "libwiiuse");
 		Native.load("com.github.boukefalos", "jlibwiiscan", "DelcomDLL");
-        Native.binary("com.github.boukefalos", "jlibwiiscan", "sox");
+        return Native.binary("com.github.boukefalos", "jlibwiiscan", "sox");
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Wiiscan.load());
 	}
 }
